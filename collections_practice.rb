5539b5f5b2539f8d6673269f66488146621cb87a
Tracie Masek <traciemasek@gmail.com>
2019-07-28 18:20:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(arr)
+  arr.all? { |el| el.downcase.start_with?('r') }
+end
+
+def contain_a(arr)
+  arr.find_all { |el| el.include?('a') }
+end
+
+def first_wa(arr)
+  arr.find { |el| el.to_s.start_with?('wa') }
+end
+
+def remove_non_strings(arr)
+  arr.find_all { |el| el.is_a?(String) }
+end
+
+def count_elements(arr)
+  arr.group_by { |el| el }.map { |k, v| k.merge(count: v.length) }
+end
+
+def merge_data(keys, data)
+  
+end
+
+def find_cool(arr)
+  arr.each do |hash|
+    hash.find_all { |k, v| v == "cool" }
+  end
+  
+end
+
+def organize_schools
+end
+
